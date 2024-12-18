0e1bd8f239928e461fdaf115ae199d9b63788224
pepsizerosugar <juntiger53@gmail.com>
2024-12-18 13:31:19 +0900

    [pepsi-010] fix: 이슈 확인을 위해 run.sh 수정

--- run.sh
+++ run.sh
@@ -33,8 +33,8 @@ fetch_release_info() {
 
     latest_version=$(echo "$LATEST_RELEASE_INFO" | jq -r '.tag_name')
     if [ -z "$latest_version" ] || [ "$latest_version" == "null" ]; then
-        error_message="최신 릴리즈 버전을 가져올 수 없습니다."
-        log_and_show_message "error" $error_message
+        error_message="최신 릴리즈 버전을 가져올 수 없습니다. URL: $api_url"
+        log_and_show_message "error" "$error_message"
         return 1
     fi
 
@@ -62,9 +62,8 @@ download_tarball() {
     log_message "info" "tarball URL: $tarball_url"
 
     log_message "info" "다운로드 중..."
-    curl -L -H "Accept: application/vnd.github.v3.raw" -o source.tar.gz "$tarball_url"
-    if [ $? -ne 0 ]; then
-        log_and_show_message "error" "tarball 다운로드 실패."
+    if ! curl -L -H "Accept: application/vnd.github.v3.raw" -o source.tar.gz "$tarball_url"; then
+        log_and_show_message "error" "tarball 다운로드 실패. 네트워크 상태를 확인하세요."
         return 1
     fi
 }
@@ -88,8 +87,7 @@ extract_tarball() {
 
     log_message "info" "압축 해제 중..."
     mkdir -p "$download_dir"
-    tar -xzf source.tar.gz -C "$download_dir" --strip-components=1
-    if [ $? -ne 0 ]; then
+    if ! tar -xzf source.tar.gz -C "$download_dir" --strip-components=1; then
         log_and_show_message "error" "압축 해제 실패. 디렉토리 '$download_dir'을 삭제합니다."
         rm -rf "$download_dir"
         return 1
@@ -98,20 +96,34 @@ extract_tarball() {
     rm -f source.tar.gz
 }
 
-run_script() {
-    cd "$download_dir" || exit
+check_tasks_permissions() {
+    log_message "info" "tasks.sh 파일과 권한 확인 중..."
 
-    log_message "info" "스크립트 실행 준비 중..."
-    chmod +x ./tasks.sh
-    if [ ! -x "./tasks.sh" ]; then
-        log_and_show_message "error" "tasks.sh 실행 파일이 없거나 실행 권한이 없습니다."
-        return 1
+    if [ ! -f "./tasks.sh" ]; then
+        log_and_show_message "error" "tasks.sh 파일이 존재하지 않습니다."
+        exit 1
     fi
 
-    chmod +x ./tasks.sh
-    ./tasks.sh
+    ls -l ./tasks.sh | log_message "info" "tasks.sh 파일 정보: $(cat -)"
+
+    if [ ! -x "./tasks.sh" ]; then
+        log_and_show_message "error" "tasks.sh 실행 권한이 없습니다."
+        exit 1
+    fi
+}
+
+run_script() {
+    cd "$download_dir" || { log_and_show_message "error" "디렉토리 '$download_dir'로 이동할 수 없습니다."; exit 1; }
+
+    log_message "info" "디렉토리 이동 성공. 현재 디렉토리: $(pwd)"
+    log_message "info" "디렉토리 파일 리스트: $(ls -l)"
+
+    log_message "info" "스크립트 실행 준비 중..."
+    check_tasks_permissions
+
+    ./tasks.sh > tasks_output.log 2>&1
     if [ $? -ne 0 ]; then
-        log_and_show_message "error" "tasks.sh 실행 실패."
+        log_and_show_message "error" "tasks.sh 실행 실패. 출력 로그를 확인하세요: $download_dir/tasks_output.log"
         return 1
     fi
 
@@ -129,4 +141,3 @@ main() {
 }
 
 main
-
